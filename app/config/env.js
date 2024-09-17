43bb64cdcb35a31f3de46616df124fc3da00e70e
luiso132413 <loviedog@miumg.edu.gt>
2024-09-16 20:46:38 -0600

    2do commit

--- app/config/env.js
+++ app/config/env.js
@@ -1,10 +1,26 @@
 
-
+/*
 const env = {
   database: 'antigua_umg2024_4441',
   username: 'antigua_umg2024_4441_user',
   password: 'nmSiYN0EyxR4gUX5vY0yQOKgHLxqBDW8',
   host: 'dpg-cqc5qld6l47c73cufe6g-a.oregon-postgres.render.com',
+  dialect: 'postgres',
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+module.exports = env;
+*/
+
+const env = {
+  database: 'antigua_umg2024_4441_user',
+  username: 'antigua_umg2024_4441_user_user',
+  password: 'tYnVvGiYQ0Leg6257ZJRhaT17J7Yw6I2',
+  host: 'dpg-cr6jnbt6l47c7394rch0-a.oregon-postgres.render.com',
   dialect: 'postgres',
   pool: {
     max: 5,
